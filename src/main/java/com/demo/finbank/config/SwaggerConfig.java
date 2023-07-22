296d290fc0476f58af12787940d5246aad81053f
My Name <efioesien@gmail.com>
2023-07-22 14:06:26 +0100

    added swagger configuration

---
+++ src/main/java/com/demo/finbank/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.demo.finbank.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customizeOpenApi(){
+
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Backend Banking App API")
+                        .version("1.1")
+                        .description("Backend Banking App API Documentation")
+                );
+
+    }
+
+
+}
--- src/main/java/com/demo/finbank/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/demo/finbank/serviceImpl/UserServiceImpl.java
@@ -1,38 +1,38 @@
-package com.demo.finbank.serviceImpl;
-
-import com.demo.finbank.dto.requests.UserRequestDto;
-import com.demo.finbank.dto.responses.BankResponse;
-import com.demo.finbank.model.User;
-import com.demo.finbank.services.UserService;
-import com.demo.finbank.util.AccountUtil;
-
-import java.math.BigDecimal;
-
-public class UserServiceImpl implements UserService {
-    @Override
-    public BankResponse createAccount(UserRequestDto userRequestDto) {
-        /**
-         * Check if User already exist else create new user
-         */
-
-        User newUser =
-        /*
-         * Create account and save a new user
-         */
-        User newUser = User.builder()
-                .firstname(userRequestDto.getFirstname())
-                .lastname(userRequestDto.getLastname())
-                .otherName(userRequestDto.getAddress())
-                .gender(userRequestDto.getGender())
-                .stateOfOrigin(userRequestDto.getStateOfOrigin())
-                .accountNumber(AccountUtil.generateAccountNumber())
-                .accountBalance(BigDecimal.ZERO)
-                .email(userRequestDto.getEmail())
-                .phoneNumber(userRequestDto.getPhoneNumber())
-                .alternativePhoneNumber(userRequestDto.getAlternativePhoneNumber())
-                .status("ACTIVE")
-                .build();
-
-        return
-    }
-}
+//package com.demo.finbank.serviceImpl;
+//
+//import com.demo.finbank.dto.requests.UserRequestDto;
+//import com.demo.finbank.dto.responses.BankResponse;
+//import com.demo.finbank.model.User;
+//import com.demo.finbank.services.UserService;
+//import com.demo.finbank.util.AccountUtil;
+//
+//import java.math.BigDecimal;
+//
+//public class UserServiceImpl implements UserService {
+//    @Override
+//    public BankResponse createAccount(UserRequestDto userRequestDto) {
+//        /**
+//         * Check if User already exist else create new user
+//         */
+//
+//        User newUser =
+//        /*
+//         * Create account and save a new user
+//         */
+//        User newUser = User.builder()
+//                .firstname(userRequestDto.getFirstname())
+//                .lastname(userRequestDto.getLastname())
+//                .otherName(userRequestDto.getAddress())
+//                .gender(userRequestDto.getGender())
+//                .stateOfOrigin(userRequestDto.getStateOfOrigin())
+//                .accountNumber(AccountUtil.generateAccountNumber())
+//                .accountBalance(BigDecimal.ZERO)
+//                .email(userRequestDto.getEmail())
+//                .phoneNumber(userRequestDto.getPhoneNumber())
+//                .alternativePhoneNumber(userRequestDto.getAlternativePhoneNumber())
+//                .status("ACTIVE")
+//                .build();
+//
+//        return
+//    }
+//}
