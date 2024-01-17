03f9d2ce502609459500e5411e0c3b172a91fb6f
Efio-esien Efiom <efio-esien@tangerine.africa>
2024-01-17 12:38:07 +0100

    updated pom file and dockerfile

--- src/main/java/com/demo/finbank/serviceImpl/EmailServiceImpl.java
+++ src/main/java/com/demo/finbank/serviceImpl/EmailServiceImpl.java
@@ -1,40 +1,40 @@
-package com.demo.finbank.serviceImpl;
-
-import com.demo.finbank.dto.EmailDetails;
-import com.demo.finbank.services.EmailService;
-//import lombok.Value;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.MailException;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-@Service
-public class EmailServiceImpl implements EmailService {
-    @Autowired
-    private JavaMailSender javaMailSender;
-
-    @Value("${spring.mail.username}")
-    private String senderEmail;
-
-    public EmailServiceImpl(JavaMailSender javaMailSender) {
-        this.javaMailSender = javaMailSender;
-    }
-
-    @Override
-    public void sendEmailAlert(EmailDetails emailDetails) {
-        try {
-            SimpleMailMessage mailMessage = new SimpleMailMessage();
-            mailMessage.setFrom(senderEmail);
-            mailMessage.setTo(emailDetails.getRecipient());
-            mailMessage.setText(emailDetails.getMessageBody());
-            mailMessage.setSubject(emailDetails.getSubject());
-
-            javaMailSender.send(mailMessage);
-            System.out.println("Email alert sent successfully");
-        } catch (MailException e) {
-            throw new RuntimeException(e);
-        }
-    }
-}
+//package com.demo.finbank.serviceImpl;
+//
+//import com.demo.finbank.dto.EmailDetails;
+//import com.demo.finbank.services.EmailService;
+////import lombok.Value;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.mail.MailException;
+//import org.springframework.mail.SimpleMailMessage;
+//import org.springframework.mail.javamail.JavaMailSender;
+//import org.springframework.stereotype.Service;
+//
+//@Service
+//public class EmailServiceImpl implements EmailService {
+//    @Autowired
+//    private JavaMailSender javaMailSender;
+//
+//    @Value("${spring.mail.username}")
+//    private String senderEmail;
+//
+//    public EmailServiceImpl(JavaMailSender javaMailSender) {
+//        this.javaMailSender = javaMailSender;
+//    }
+//
+//    @Override
+//    public void sendEmailAlert(EmailDetails emailDetails) {
+//        try {
+//            SimpleMailMessage mailMessage = new SimpleMailMessage();
+//            mailMessage.setFrom(senderEmail);
+//            mailMessage.setTo(emailDetails.getRecipient());
+//            mailMessage.setText(emailDetails.getMessageBody());
+//            mailMessage.setSubject(emailDetails.getSubject());
+//
+//            javaMailSender.send(mailMessage);
+//            System.out.println("Email alert sent successfully");
+//        } catch (MailException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//}
